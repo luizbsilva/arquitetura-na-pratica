b8d0f16ed311eb34e736da7404a80a07bfd1caa9
José Luiz <luizbsilva@gmail.com>
2024-02-17 15:09:27 -0300

    feat: Criacao do Projeto

---
+++ Pods/Target Support Files/Pods-SwiftArquiteturas-SwiftArquiteturasUITests/Pods-SwiftArquiteturas-SwiftArquiteturasUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftArquiteturas_SwiftArquiteturasUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftArquiteturas_SwiftArquiteturasUITests
+@end
---
+++ SwiftArquiteturas/Source/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftArquiteturas
+//
+//  Created by Jose Luiz Bento da Silva on 17/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
